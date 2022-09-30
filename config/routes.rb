8199b57b3fbceea262744bbb4d9f32f73f22f102
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2022-09-30 15:18:31 +0200

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
     match 'pept2filtered', via: %i[get post], to: 'mpa#pept2filtered'
   end
 
+  scope :datasets, as: 'datasets' do
+    match 'sampledata', via: [:post], to: 'datasets#sampledata'
+  end
+
   namespace :api, path: 'api/v1' do
     match 'pept2taxa' => "api#pept2taxa", via: %i[get post]
     match 'pept2lca' => "api#pept2lca", via: %i[get post]
@@ -45,14 +49,5 @@ Rails.application.routes.draw do
     match 'peptinfo' => 'api#peptinfo', via: %i[get post]
     match 'taxonomy' => 'api#taxonomy', via: %i[get post]
     match 'messages' => 'api#messages', via: %i[get post]
-  end
-
-  scope :mpa, as: 'mpa' do
-    match '/*path', via: [:options], to: 'handle_options#handle_options_request'
-    match 'pept2data', via: %i[get post], to: 'mpa#pept2data'
-  end
-
-  scope :datasets, as: 'datasets' do
-    match 'sampledata', via: [:post], to: 'datasets#sampledata'
   end
 end

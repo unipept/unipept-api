7e1dc9847f7fa17d7d574df66e9a949133f2bfe0
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2023-07-28 11:54:47 +0200

    Remove RVM custom path

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -4,7 +4,7 @@ set :deploy_to, '/home/unipept/rails'
 
 set :server, ENV['server'] || 'patty.ugent.be'
 
-set :rvm_custom_path, '/usr/share/rvm'
+#set :rvm_custom_path, '/usr/share/rvm'
 
 # don't specify db as it's not needed for unipept
 server "#{fetch(:server)}", user: 'unipept', roles: %i[web app], ssh_options: {

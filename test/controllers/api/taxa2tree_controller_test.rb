2ec7a12d437a03db15df3a8ff15b47ecb81a917e
tibvdm <tibovandemoortele@hotmail.com>
2024-03-20 09:34:40 +0100

    disable module length metric

--- test/controllers/api/taxa2tree_controller_test.rb
+++ test/controllers/api/taxa2tree_controller_test.rb
@@ -15,31 +15,6 @@ class Api::Taxa2treeControllerTest < ActionController::TestCase
     get :taxa2tree, params: { input: %w[14], format: 'json' }
   end
 
-  test 'should get taxa2tree with names' do
-    @expected = '{
-      "id":1,"name":"Organism","rank":"no rank","data":{"count":1,"self_count":0},"children":[
-        {"id":13,"name":"kingdom2","rank":"kingdom","data":{"count":1,"self_count":0},"children":[
-          {"id":14,"name":"phylum1","rank":"phylum","data":{"count":1,"self_count":1},"children":[]}
-        ]}
-      ]
-    }'
-
-    get :taxa2tree, params: { input: %w[14], names: 'true', format: 'json' }
-    puts @response.body
-  end
-
-  # test 'should get taxa2tree with extra' do
-  #   @expected = '{
-  #     "id":1,"name":"Organism","rank":"no rank","data":{"count":1,"self_count":0},"children":[
-  #       {"id":13,"name":"kingdom2","rank":"kingdom","data":{"count":1,"self_count":0},"children":[
-  #         {"id":14,"name":"phylum1","rank":"phylum","data":{"count":1,"self_count":1},"children":[]}
-  #       ]}
-  #     ]
-  #   }'
-
-  #   get :taxa2tree, params: { input: %w[14], extra: 'true', format: 'json' }
-  # end
-
   private
 
   def assert_success

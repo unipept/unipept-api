232bb3c0518bbbe2943c5dd1be35103e26c6f596
tibvdm <tibovandemoortele@hotmail.com>
2024-04-15 23:43:05 +0200

    test

--- app/controllers/mpa/pept2data_controller.rb
+++ app/controllers/mpa/pept2data_controller.rb
@@ -7,45 +7,27 @@ class Mpa::Pept2dataController < Mpa::MpaController
     @equate_il = params[:equate_il].nil? ? true : params[:equate_il] == 'true'
 
     @response = Hash.new
-    @lineages = Hash.new
 
     # Request the suffix array search service
     search_results = search(peptides, @equate_il)
 
-    @response = search_results["result"].map do |entry|
-      entry.delete("taxa")
-      entry.delete("uniprot_accessions")
-      entry
+    taxa = []
+    search_results["result"].each do |result|
+      @response[result["sequence"]] = {
+        sequence: result["sequence"],
+        lca: result["lca"],
+        fa: result["fa"]
+      }
+      taxa.append(result["lca"])
     end
 
-    # proteins = Set.new
-    # search_results["result"].each do |result|
-    #   proteins.merge(result['uniprot_accessions'])
-    # end
+    lineages = Hash.new
+    Lineage.find(taxa).each do |lineage|
+      lineages[lineage.taxon_id] = lineage.to_a_idx
+    end
 
-    # entries = UniprotEntry.where(uniprot_accession_number: proteins.to_a.uniq)
-
-    # # Convert the retrieved entries to a hash (for easy retrieval)
-    # accession_to_protein = Hash.new
-    # entries.each do |entry|
-    #   accession_to_protein[entry.uniprot_accession_number] = entry
-    # end
-
-    # taxa = []
-    # search_results["result"].each do |result|
-    #   uniprot_entries = result["uniprot_accessions"].map { |acc| accession_to_protein[acc] }
-    #   result["fa"] = UniprotEntry.summarize_fa(uniprot_entries)
-    #   @response[result["sequence"]] = result
-    #   taxa.append(result["lca"])
-    # end
-
-    # looked_up_lineages = Lineage.find(taxa)
-    # looked_up_lineages.each do |lineage|
-    #   @lineages[lineage.taxon_id] = lineage.to_a_idx
-    # end
-
-    # @response.each do |_, entry|
-    #   entry["lineage"] = @lineages[entry["lca"].to_i]
-    # end
+    @response.each do |_, value|
+      entry["lineage"] = @lineages[value["lca"].to_i]
+    end
   end
 end

65d26eeb6129d31f26cdfe51cf379d4414dec455
tibvdm <tibovandemoortele@hotmail.com>
2024-04-15 23:46:42 +0200

    test

--- app/controllers/mpa/pept2data_controller.rb
+++ app/controllers/mpa/pept2data_controller.rb
@@ -27,7 +27,7 @@ class Mpa::Pept2dataController < Mpa::MpaController
     end
 
     @response.each do |_, value|
-      value["lineage"] = @lineages[value["lca"].to_i]
+      value["lineage"] = lineages[value["lca"].to_i]
     end
   end
 end

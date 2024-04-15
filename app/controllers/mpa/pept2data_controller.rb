d237786e7de166548a5552fb34ae5c5f8ef2e039
tibvdm <tibovandemoortele@hotmail.com>
2024-04-16 00:36:12 +0200

    test

--- app/controllers/mpa/pept2data_controller.rb
+++ app/controllers/mpa/pept2data_controller.rb
@@ -21,15 +21,44 @@ class Mpa::Pept2dataController < Mpa::MpaController
       taxa.append(result["lca"])
     end
 
-    lineages = Hash.new
+    @lineages = Hash.new
     Lineage.find(taxa).each do |lineage|
-      lineages[lineage.taxon_id.to_i] = lineage.to_a_idx
+      @lineages[lineage.taxon_id] = lineage.to_a_idx
     end
-
-    @lineages = lineages
 
     @response.each do |_, value|
-      value["lineage"] = lineages[value["lca"].to_i]
+      entry["test"] = value
+      entry["lineage"] = @lineages[value["lca"].to_i]
     end
+
+    # proteins = Set.new
+    # search_results["result"].each do |result|
+    #   proteins.merge(result['uniprot_accessions'])
+    # end
+
+    # entries = UniprotEntry.where(uniprot_accession_number: proteins.to_a.uniq)
+
+    # # Convert the retrieved entries to a hash (for easy retrieval)
+    # accession_to_protein = Hash.new
+    # entries.each do |entry|
+    #   accession_to_protein[entry.uniprot_accession_number] = entry
+    # end
+
+    # taxa = []
+    # search_results["result"].each do |result|
+    #   uniprot_entries = result["uniprot_accessions"].map { |acc| accession_to_protein[acc] }
+    #   result["fa"] = UniprotEntry.summarize_fa(uniprot_entries)
+    #   @response[result["sequence"]] = result
+    #   taxa.append(result["lca"])
+    # end
+
+    # looked_up_lineages = Lineage.find(taxa)
+    # looked_up_lineages.each do |lineage|
+    #   @lineages[lineage.taxon_id] = lineage.to_a_idx
+    # end
+
+    # @response.each do |_, entry|
+    #   entry["lineage"] = @lineages[entry["lca"].to_i]
+    # end
   end
 end

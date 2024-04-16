2398dcea85a137c2449da13aab93d9e9ffcf0e9b
tibvdm <tibovandemoortele@hotmail.com>
2024-04-16 10:57:19 +0200

    test

--- app/helpers/suffix_array_helper.rb
+++ app/helpers/suffix_array_helper.rb
@@ -1,7 +1,7 @@
 module SuffixArrayHelper
   def search(peptides, equate_il)
     # Convert the peptides array into a json string
-    json_data = { peptides: peptides }.to_json
+    json_data = { peptides: peptides, equalize_I_and_L: equate_il }.to_json
 
     # The URL to the suffix array search service
     url = URI.parse("http://localhost:3000/search")

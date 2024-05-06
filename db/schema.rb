edc40faa471c6876dfb0f0ca1e5571e66918dcf1
stijndcl <declercq.stijn@outlook.com>
2024-05-06 16:18:20 +0200

    Add auto increment

--- db/schema.rb
+++ db/schema.rb
@@ -20,58 +20,58 @@ ActiveRecord::Schema[7.0].define(version: 0) do
   create_enum "go_namespace", ["biological process", "molecular function", "cellular component"]
   create_enum "rank_type", ["no rank", "superkingdom", "kingdom", "subkingdom", "superphylum", "phylum", "subphylum", "superclass", "class", "subclass", "superorder", "order", "suborder", "infraorder", "superfamily", "family", "subfamily", "tribe", "subtribe", "genus", "subgenus", "species group", "species subgroup", "species", "subspecies", "strain", "varietas", "forma"]
 
-  create_table "dataset_items", id: :integer, default: nil, force: :cascade do |t|
+  create_table "dataset_items", id: :integer, auto_increment: true, force: :cascade do |t|
     t.bigint "dataset_id"
     t.string "name", limit: 160
     t.text "data", null: false
     t.integer "order"
   end
 
-  create_table "datasets", id: :integer, default: nil, force: :cascade do |t|
+  create_table "datasets", id: :integer, auto_increment: true, force: :cascade do |t|
     t.string "environment", limit: 160
     t.string "reference", limit: 500
     t.string "url", limit: 200
     t.string "project_website", limit: 200
   end
 
-  create_table "ec_cross_references", id: :bigint, default: nil, force: :cascade do |t|
+  create_table "ec_cross_references", id: :bigint, auto_increment: true, force: :cascade do |t|
     t.bigint "uniprot_entry_id", null: false
     t.string "ec_number_code", limit: 15, null: false
     t.index ["uniprot_entry_id"], name: "idx_ec_cross_references_uniprot_entry_id"
   end
 
-  create_table "ec_numbers", id: :integer, default: nil, force: :cascade do |t|
+  create_table "ec_numbers", id: :integer, auto_increment: true, force: :cascade do |t|
     t.string "code", limit: 15, null: false
     t.string "name", limit: 155, null: false
     t.index ["code"], name: "idx_ec_numbers_code"
   end
 
-  create_table "go_cross_references", id: :bigint, default: nil, force: :cascade do |t|
+  create_table "go_cross_references", id: :bigint, auto_increment: true, force: :cascade do |t|
     t.bigint "uniprot_entry_id", null: false
     t.string "go_term_code", limit: 15, null: false
     t.index ["uniprot_entry_id"], name: "idx_go_cross_references_uniprot_entry_id"
   end
 
-  create_table "go_terms", id: :integer, default: nil, force: :cascade do |t|
+  create_table "go_terms", id: :integer, auto_increment: true, force: :cascade do |t|
     t.string "code", limit: 15, null: false
     t.enum "namespace", null: false, enum_type: "go_namespace"
     t.string "name", limit: 200, null: false
     t.index ["code"], name: "idx_go_terms_code"
   end
 
-  create_table "interpro_cross_references", id: :bigint, default: nil, force: :cascade do |t|
+  create_table "interpro_cross_references", id: :bigint, auto_increment: true, force: :cascade do |t|
     t.bigint "uniprot_entry_id", null: false
     t.string "interpro_entry_code", limit: 9, null: false
     t.index ["uniprot_entry_id"], name: "idx_interpro_cross_references_uniprot_entry_id"
   end
 
-  create_table "interpro_entries", id: :integer, default: nil, force: :cascade do |t|
+  create_table "interpro_entries", id: :integer, auto_increment: true, force: :cascade do |t|
     t.string "code", limit: 9, null: false
     t.string "category", limit: 32, null: false
     t.string "name", limit: 160, null: false
   end
 
-  create_table "lineages", primary_key: "taxon_id", id: :integer, default: nil, force: :cascade do |t|
+  create_table "lineages", primary_key: "taxon_id", id: :integer, auto_increment: true, force: :cascade do |t|
     t.integer "superkingdom"
     t.integer "kingdom"
     t.integer "subkingdom"
@@ -101,7 +101,7 @@ ActiveRecord::Schema[7.0].define(version: 0) do
     t.integer "forma"
   end
 
-  create_table "peptides", id: :bigint, default: nil, force: :cascade do |t|
+  create_table "peptides", id: :bigint, auto_increment: true, force: :cascade do |t|
     t.bigint "sequence_id", null: false
     t.bigint "original_sequence_id", null: false
     t.bigint "uniprot_entry_id", null: false
@@ -110,7 +110,7 @@ ActiveRecord::Schema[7.0].define(version: 0) do
     t.index ["uniprot_entry_id"], name: "idx_peptides_uniprot_entry_id"
   end
 
-  create_table "sequences", id: :bigint, default: nil, force: :cascade do |t|
+  create_table "sequences", id: :bigint, auto_increment: true, force: :cascade do |t|
     t.string "sequence", limit: 50, null: false
     t.integer "lca"
     t.integer "lca_il"
@@ -121,14 +121,14 @@ ActiveRecord::Schema[7.0].define(version: 0) do
     t.index ["sequence"], name: "idx_sequences_sequence"
   end
 
-  create_table "taxons", id: :integer, default: nil, force: :cascade do |t|
+  create_table "taxons", id: :integer, auto_increment: true, force: :cascade do |t|
     t.string "name", limit: 120, null: false
     t.enum "rank", enum_type: "rank_type"
     t.integer "parent_id"
     t.integer "valid_taxon", limit: 2, default: 1, null: false
   end
 
-  create_table "uniprot_entries", id: :integer, default: nil, force: :cascade do |t|
+  create_table "uniprot_entries", id: :integer, auto_increment: true, force: :cascade do |t|
     t.string "uniprot_accession_number", limit: 10, null: false
     t.integer "version", null: false
     t.integer "taxon_id", null: false

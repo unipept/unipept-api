fe31f38c0cf7f2e361a38428e409640532da7979
tibvdm <tibovandemoortele@hotmail.com>
2024-07-03 11:48:44 +0200

    accept all formats like Rails

--- api/src/controllers/mod.rs
+++ api/src/controllers/mod.rs
@@ -1,4 +1,5 @@
-use axum::{async_trait, extract::FromRequestParts, http::{request::Parts, StatusCode}};
+use axum::{async_trait, extract::{FromRequest, FromRequestParts, Multipart, RawForm, Request}, http::{header::CONTENT_TYPE, request::Parts, StatusCode}, response::{IntoResponse, Response}, Json, RequestExt};
+use serde::de::DeserializeOwned;
 
 pub mod api;
 pub mod datasets;
@@ -17,7 +18,75 @@ where
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
         let query = parts.uri.query().ok_or((StatusCode::BAD_REQUEST, "missing query string"))?;
-        Ok(Self(serde_qs::from_str(query).map_err(|_| (StatusCode::BAD_REQUEST, "invalid query string"))?))
+        Ok(Self(serde_qs::from_str(query).map_err(|err| {
+            eprintln!("{:?}", err);
+            (StatusCode::BAD_REQUEST, "invalid query string")
+        })?))
+    }
+}
+
+pub struct Form<T>(T);
+
+#[async_trait]
+impl<S, T> FromRequest<S> for Form<T>
+where
+    S: Send + Sync,
+    T: serde::de::DeserializeOwned,
+{
+    type Rejection = Response;
+
+    async fn from_request(req: Request, _state: &S) -> Result<Self, Self::Rejection> {
+        let RawForm(form) = req.extract().await.map_err(IntoResponse::into_response)?;
+
+        let form_bytes = form.to_vec();
+        let decoded_bytes = urlencoding::decode_binary(&form_bytes);
+
+        Ok(Self(serde_qs::from_bytes(&decoded_bytes).map_err(|_| StatusCode::BAD_REQUEST.into_response())?))
+    }
+}
+
+pub struct PostContent<T>(T);
+
+#[async_trait]
+impl<S, T> FromRequest<S> for PostContent<T>
+where
+    S: Send + Sync,
+    Json<T>: FromRequest<()>,
+    Form<T>: FromRequest<()>,
+    T: 'static + DeserializeOwned,
+{
+    type Rejection = Response;
+
+    async fn from_request(req: Request, _state: &S) -> Result<Self, Self::Rejection> {
+        let content_type_header = req.headers().get(CONTENT_TYPE);
+        let content_type = content_type_header.and_then(|value| value.to_str().ok());
+
+        if let Some(content_type) = content_type {
+            if content_type.starts_with("application/json") {
+                let Json(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
+                return Ok(Self(payload));
+            }
+
+            if content_type.starts_with("application/x-www-form-urlencoded") {
+                let Form(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
+                return Ok(Self(payload));
+            }
+
+            if content_type.starts_with("multipart/form-data") {
+                let mut multipart = Multipart::from_request(req, _state).await.map_err(IntoResponse::into_response)?;
+
+                let mut querystring = String::new();
+                while let Some(field) = multipart.next_field().await.unwrap() {
+                    let name = field.name().unwrap().to_string();
+                    let value = field.text().await.unwrap();
+                    querystring.push_str(&format!("{}={}&", name, value));
+                }
+
+                return Ok(Self(serde_qs::from_str(&querystring).map_err(|_| StatusCode::BAD_REQUEST.into_response())?));
+            }
+        }
+
+        Err(StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response())
     }
 }
 
@@ -64,7 +133,7 @@ macro_rules! generate_handlers {
 
         pub async fn post_handler(
             state: State<$state_type>,
-            Json(params): Json<$params_type>
+            $crate::controllers::PostContent(params): $crate::controllers::PostContent<$params_type>
         ) -> Json<$ret> {
             $handler_name(state, params).await
         }
@@ -95,7 +164,7 @@ macro_rules! generate_handlers {
 
                 pub async fn [<post_handler_ $version:lower>](
                     state: State<$state_type>,
-                    Json(params): Json<$params_type>
+                    $crate::controllers::PostContent(params): $crate::controllers::PostContent<$params_type>
                 ) -> Json<$ret> {
                     $handler_name(state, params, $version).await
                 }
--- api/src/routes.rs
+++ api/src/routes.rs
@@ -30,54 +30,77 @@ fn create_api_routes() -> Router<AppState> {
 }
 
 fn create_api_v1_routes() -> Router<AppState> {
-    Router::new()
-        .route("/pept2ec", get(pept2ec::get_handler).post(pept2ec::post_handler))
-        .route("/pept2funct", get(pept2funct::get_handler).post(pept2funct::post_handler))
-        .route("/pept2go", get(pept2go::get_handler).post(pept2go::post_handler))
-        .route("/pept2interpro", get(pept2interpro::get_handler).post(pept2interpro::post_handler))
-        .route("/pept2lca", get(pept2lca::get_handler_v1).post(pept2lca::post_handler_v1))
-        .route("/pept2prot", get(pept2prot::get_handler).post(pept2prot::post_handler))
-        .route("/pept2taxa", get(pept2taxa::get_handler_v1).post(pept2taxa::post_handler_v1))
-        .route("/peptinfo", get(peptinfo::get_handler_v1).post(peptinfo::post_handler_v1))
-        .route("/protinfo", get(protinfo::get_handler_v1).post(protinfo::post_handler_v1))
-        .route("/taxa2lca", get(taxa2lca::get_handler_v1).post(taxa2lca::post_handler_v1))
-        .route("/taxa2tree", get(taxa2tree::handler).post(taxa2tree::handler)) // TODO: implement
-        .route("/taxonomy", get(taxonomy::get_handler_v1).post(taxonomy::post_handler_v1))
+    define_routes!(
+        "/pept2ec", get(pept2ec::get_handler).post(pept2ec::post_handler),
+        "/pept2funct", get(pept2funct::get_handler).post(pept2funct::post_handler),
+        "/pept2go", get(pept2go::get_handler).post(pept2go::post_handler),
+        "/pept2interpro", get(pept2interpro::get_handler).post(pept2interpro::post_handler),
+        "/pept2lca", get(pept2lca::get_handler_v1).post(pept2lca::post_handler_v1),
+        "/pept2prot", get(pept2prot::get_handler).post(pept2prot::post_handler),
+        "/pept2taxa", get(pept2taxa::get_handler_v1).post(pept2taxa::post_handler_v1),
+        "/peptinfo", get(peptinfo::get_handler_v1).post(peptinfo::post_handler_v1),
+        "/protinfo", get(protinfo::get_handler_v1).post(protinfo::post_handler_v1),
+        "/taxa2lca", get(taxa2lca::get_handler_v1).post(taxa2lca::post_handler_v1),
+        "/taxa2tree", get(taxa2tree::handler).post(taxa2tree::handler), // TODO: implement
+        "/taxonomy", get(taxonomy::get_handler_v1).post(taxonomy::post_handler_v1)
+    )
 }
 
 fn create_api_v2_routes() -> Router<AppState> {
-    Router::new()
-        .route("/pept2ec", get(pept2ec::get_handler).post(pept2ec::post_handler))
-        .route("/pept2funct", get(pept2funct::get_handler).post(pept2funct::post_handler))
-        .route("/pept2go", get(pept2go::get_handler).post(pept2go::post_handler))
-        .route("/pept2interpro", get(pept2interpro::get_handler).post(pept2interpro::post_handler))
-        .route("/pept2lca", get(pept2lca::get_handler_v2).post(pept2lca::post_handler_v2))
-        .route("/pept2prot", get(pept2prot::get_handler).post(pept2prot::post_handler))
-        .route("/pept2taxa", get(pept2taxa::get_handler_v2).post(pept2taxa::post_handler_v2))
-        .route("/peptinfo", get(peptinfo::get_handler_v2).post(peptinfo::post_handler_v2))
-        .route("/protinfo", get(protinfo::get_handler_v2).post(protinfo::post_handler_v2))
-        .route("/taxa2lca", get(taxa2lca::get_handler_v2).post(taxa2lca::post_handler_v2))
-        .route("/taxa2tree", get(taxa2tree::handler).post(taxa2tree::handler)) // TODO: implement
-        .route("/taxonomy", get(taxonomy::get_handler_v2).post(taxonomy::post_handler_v2))
+    define_routes!(
+        "/pept2ec", get(pept2ec::get_handler).post(pept2ec::post_handler),
+        "/pept2funct", get(pept2funct::get_handler).post(pept2funct::post_handler),
+        "/pept2go", get(pept2go::get_handler).post(pept2go::post_handler),
+        "/pept2interpro", get(pept2interpro::get_handler).post(pept2interpro::post_handler),
+        "/pept2lca", get(pept2lca::get_handler_v2).post(pept2lca::post_handler_v2),
+        "/pept2prot", get(pept2prot::get_handler).post(pept2prot::post_handler),
+        "/pept2taxa", get(pept2taxa::get_handler_v2).post(pept2taxa::post_handler_v2),
+        "/peptinfo", get(peptinfo::get_handler_v2).post(peptinfo::post_handler_v2),
+        "/protinfo", get(protinfo::get_handler_v2).post(protinfo::post_handler_v2),
+        "/taxa2lca", get(taxa2lca::get_handler_v2).post(taxa2lca::post_handler_v2),
+        "/taxa2tree", get(taxa2tree::handler).post(taxa2tree::handler), // TODO: implement
+        "/taxonomy", get(taxonomy::get_handler_v2).post(taxonomy::post_handler_v2)
+    )
 }
 
 fn create_datasets_routes() -> Router<AppState> {
-    Router::new()
-        .route("/sampledata", get(sampledata::get_handler).post(sampledata::post_handler))
+    define_routes!(
+        "/sampledata", get(sampledata::get_handler).post(sampledata::post_handler)
+    )
 }
 
 fn create_mpa_routes() -> Router<AppState> {
-    Router::new()
-        .route("/pept2data", get(pept2data::get_handler).post(pept2data::post_handler))
-        .route("/pept2filtered", get(pept2filtered::get_handler).post(pept2filtered::post_handler))
+    define_routes!(
+        "/pept2data", get(pept2data::get_handler).post(pept2data::post_handler),
+        "/pept2filtered", get(pept2filtered::get_handler).post(pept2filtered::post_handler)
+    )
 }
 
 fn create_private_api_routes() -> Router<AppState> {
-    Router::new()
-        .route("/ecnumbers", get(ecnumbers::get_handler).post(ecnumbers::post_handler))
-        .route("/goterms", get(goterms::get_handler).post(goterms::post_handler))
-        .route("/interpros", get(interpros::get_handler).post(interpros::post_handler))
-        .route("/metadata", get(metadata::get_handler).post(metadata::post_handler))
-        .route("/proteins", get(proteins::get_handler).post(proteins::post_handler))
-        .route("/taxa", get(taxa::get_handler).post(taxa::post_handler))
+    define_routes!(
+        "/ecnumbers", get(ecnumbers::get_handler).post(ecnumbers::post_handler),
+        "/goterms", get(goterms::get_handler).post(goterms::post_handler),
+        "/interpros", get(interpros::get_handler).post(interpros::post_handler),
+        "/metadata", get(metadata::get_handler).post(metadata::post_handler),
+        "/proteins", get(proteins::get_handler).post(proteins::post_handler),
+        "/taxa", get(taxa::get_handler).post(taxa::post_handler)
+    )
 }
+
+macro_rules! define_routes {
+    (
+        $( $path:tt, $handlers:expr),*
+    ) => {{
+        let mut router = Router::new();
+
+        $(
+            router = router
+                .route($path, $handlers)
+                .route(concat!($path, ".json"), $handlers);
+        )*
+
+        router
+    }};
+}
+
+pub(crate) use define_routes;

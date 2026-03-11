cb2fa71be1a24f1982fadd5e3f817b0e01a76e21
tibvdm <tibovandemoortele@hotmail.com>
2026-03-11 16:55:02 +0100

    Debug

--- index/src/lib.rs
+++ index/src/lib.rs
@@ -1,13 +1,12 @@
 pub use errors::IndexError;
 use errors::LoadIndexError;
-use sa_server::load_suffix_array_file;
+use sa_server::{load_proteins_file, load_suffix_array_file};
 pub use sa_index::peptide_search::ProteinInfo;
 pub use sa_index::peptide_search::SearchResult;
 use sa_index::{
     peptide_search::{search_all_peptides},
     sa_searcher::BitVecSearcher,
 };
-use sa_mappings::proteins::Proteins;
 
 mod errors;
 
@@ -18,10 +17,8 @@ pub struct Index {
 impl Index {
     pub fn try_from_files(index_file: &str, proteins_file: &str) -> Result<Self, IndexError> {
         eprintln!("Loading proteins from file: {}", proteins_file);
-        let proteins = Proteins::try_from_database_file(proteins_file)
-            .map_err(|_| LoadIndexError::LoadProteinsErrors(
-                LoadIndexError::FileNotFound(proteins_file.to_string()).to_string(),
-            ))?;
+        let proteins =
+            load_proteins_file(proteins_file, false).map_err(|err| LoadIndexError::LoadProteinsErrors(err.to_string()))?;
 
         eprintln!("Loading suffix array from file: {}", index_file);
         let suffix_array =
